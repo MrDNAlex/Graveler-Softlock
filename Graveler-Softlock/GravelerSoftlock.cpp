2f26819bddc625a9cdd903c2d7ff6f4106f118b5
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-08-11 03:01:22 -0400

    Finished!

--- Graveler-Softlock/GravelerSoftlock.cpp
+++ Graveler-Softlock/GravelerSoftlock.cpp
@@ -1,8 +1,13 @@
 #include "BattleSimulation.cuh"
-#include "BattleSimulationOptimized.cuh"
 #include <chrono>
 
-
+/// <summary>
+/// Simulates Battles on the GPU and Determines the Max number of Paralysis Rolls
+/// </summary>
+/// <param name="iterations"> The Number of Battles to Simulate </param>
+/// <param name="numOfTurns"> Number of Turns in a Battle </param>
+/// <param name="numOfPossibleMoves"> The Number of possible moves or turns in a Turn </param>
+/// <param name="rngSeed"> The Seed for the RNG Numbers </param>
 void GetMaxParalysis(int iterations, int numOfTurns, int numOfPossibleMoves, unsigned long long rngSeed)
 {
 	//Start the Clock!
@@ -21,34 +26,51 @@ void GetMaxParalysis(int iterations, int numOfTurns, int numOfPossibleMoves, uns
 	printf("Max Paralysis Count in %f seconds: %d times\n", duration.count(), moveRolls);
 }
 
-int GetMaxParalysisOptimized(int iterations)
+/// <summary>
+/// Determines if there are Active GPUs on the Computer
+/// </summary>
+/// <returns> True if GPUs are Detected, False otherwise </returns>
+bool IsGPUActive()
 {
-	//Start the Clock!
-	auto before = std::chrono::high_resolution_clock::now();
+	int nDevices;
+	cudaGetDeviceCount(&nDevices);
+	for (int i = 0; i < nDevices; i++) {
+		cudaDeviceProp prop;
+		cudaGetDeviceProperties(&prop, i);
+		std::cout << "Device Number: " << i << std::endl;
+		std::cout << "  Device name: " << prop.name << std::endl;
+		std::cout << "  Multiprocessors: " << prop.multiProcessorCount << std::endl;
+		std::cout << "  Max threads per multiprocessor: " << prop.maxThreadsPerMultiProcessor << std::endl;
+		std::cout << "  Max threads per block: " << prop.maxThreadsPerBlock << std::endl;
+		std::cout << "  Max block dimensions: [" << prop.maxThreadsDim[0] << ", " << prop.maxThreadsDim[1] << ", " << prop.maxThreadsDim[2] << "]" << std::endl;
+		std::cout << "  Max grid dimensions: [" << prop.maxGridSize[0] << ", " << prop.maxGridSize[1] << ", " << prop.maxGridSize[2] << "]" << std::endl;
 
-	//Simulate Battles on GPU
-	int paralysisCount = SimulateBattlesOptimized(iterations);
+		printf("\n");
+	}
 
-	//Stop the Clock!
-	auto after = std::chrono::high_resolution_clock::now();
-
-	//Calculate the time taken
-	auto duration = std::chrono::duration_cast<std::chrono::duration<double>>(after - before);
-
-	printf("Max Paralysis Count in %f seconds: %d times\n", duration.count(), paralysisCount);
-
-	return paralysisCount;
+	if (nDevices == 0)
+	{
+		std::cout << "No CUDA devices found!" << std::endl;
+		return false;
+	}
+	else
+		return true;
 }
-
 
 
 int main()
 {
+	if (!IsGPUActive())
+	{
+		std::cout << "GPU not Detected, Program will not function without one" << std::endl;
+		return 1;
+	}
+
 	//int iterations = 1000000000;
-	int iterations = 100000000;
-	int numOfPossibleMoves = 4;
-	int numOfTurns = 231;
-	unsigned long long RNGSeed = 12346752378;
+	int iterations = 0;
+	int numOfPossibleMoves = 0;
+	int numOfTurns = 0;
+	unsigned long long RNGSeed = 0;
 
 	std::cout << "Enter the Number of Iterations: ";
 	std::cin >> iterations; 
@@ -63,8 +85,6 @@ int main()
 	std::cin >> RNGSeed;
 
 	GetMaxParalysis(iterations, numOfTurns, numOfPossibleMoves, RNGSeed);
-	GetMaxParalysisOptimized(iterations);
-
 
 	return 0;
 }
